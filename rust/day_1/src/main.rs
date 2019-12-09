efb746ca72a34cdce3c1bf014e6b2910861d0746
Lewis Weinberger <lhw28@cam.ac.uk>
2019-12-09 09:49:54 +0000

    added rust day 1 part 2 solution

--- rust/day_1/src/main.rs
+++ rust/day_1/src/main.rs
@@ -1,7 +1,12 @@
 ///
 /// --- Day 1: The Tyranny of the Rocket Equation ---
 ///
-/// What is the sum of the fuel requirements for all of the modules on your spacecraft?
+/// Part 1) What is the sum of the fuel requirements for all of the
+/// modules on your spacecraft?
+///
+/// Part 2) What is the sum of the fuel requirements for all of the
+/// modules on your spacecraft when also taking into account the mass
+/// of the added fuel?
 /// 
 /// -------------------------------------------------
 ///
@@ -17,12 +22,26 @@ fn main() -> Result<(), std::io::Error> {
         .filter_map(|arg| arg.parse::<isize>().ok())
         .collect();
 
-    // Accumulate total fuel
-    let total = input.iter().fold(0, |sum, x| sum + (x / 3) - 2);
+    // Part 1) Accumulate total fuel
+    let mut total = input.iter().fold(0, |sum, x| sum + (x / 3) - 2);
+    println!("Part 1) Total required fuel = {}", total);
 
-    // Print result
-    println!("Total required fuel = {}", total);
+    // Part 2) Accumulate total fuel including mass of added fuel
+    total = input.iter().fold(0, |sum, x| sum + fuel(0, *x));
+    println!("Part 2) Total required fuel = {}", total);
 
     // Finish
     Ok(())
+}
+
+fn fuel(total: isize, remaining: isize) -> isize {
+    // Calculate required fuel for remaining mass
+    let input = (remaining / 3) - 2;
+
+    // Check if any more fuel needs to be accounted for
+    if input <= 0 {
+        total
+    } else {
+        fuel(total + input, input)
+    }
 }
