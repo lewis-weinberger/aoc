8f62c735517f48ac52d83cc8fd94a5b01d9d04b4
Lewis Weinberger <lweinberger@btinternet.com>
2019-12-12 22:09:55 +0000

    finished day 2 part 2 in scheme

--- scheme/day_2.scm
+++ scheme/day_2.scm
@@ -15,17 +15,23 @@
 
 ;; Replace Kth entry in LST with NEW
 (define (replace-val lst k new)
-  (cond ((null? lst) '())
-        ((= k 0) (cons new (cdr lst)))
-	(else (cons (car lst) (replace-val (cdr lst) (- k 1) new)))))
-
+  (if (< k (length lst))
+      (cond ((null? lst) '())
+	    ((= k 0) (cons new (cdr lst)))
+	    (else (cons (car lst) (replace-val (cdr lst) (- k 1) new))))
+      '()))
+  
 ;; Overide positions 1 and 2 in CODE with NOUN and VERB
 (define (program-alarm code noun verb)
   (replace-val (replace-val code 1 noun) 2 verb))
 
-;; Perform operation F on CODE at addresses A, B and C
+;; Return memory pointed to by address at position X in CODE
+(define (address code x)
+  (list-ref code (list-ref code x)))
+
+;; Perform operation F on CODE at addresses in positions A, B and C
 (define (op f code a b c)
-  (replace-val code c (f (list-ref code a) (list-ref code b))))
+  (replace-val code (list-ref code c) (f (address code a) (address code b))))
 
 ;; Execute program from CODE at instruction pointer IP
 (define (run code ip)
@@ -36,7 +42,9 @@
 	;; Intcode 2 is multiplication
 	((2) (run (op * code (+ ip 1) (+ ip 2) (+ ip 3)) (+ ip 4)))
 	;; Intcode 99 halts the program, returning the value at address 0
-	((99) (list-ref code 0)))
+	((99) (list-ref code 0))
+	;; No other intcodes specified
+	(else (list-ref code 0)))
       ;; Reached end of program without halting
       (list-ref code 0)))
 
@@ -48,10 +56,19 @@
 (define (part-one code)
   (run (program-alarm code 12 2) 0))
 
+;; Recurse over possible nouns and verbs
+(define (noun-verb-iter code noun verb)
+  (if (<= verb 99)
+      (if (= (run (program-alarm code noun verb) 0) 19690720)
+	   (values noun verb (+ (* 100 noun) verb))
+           (noun-verb-iter code noun (+ verb 1)))
+      (if (<= noun 99)
+	  (noun-verb-iter code (+ noun 1) 0)
+	  (values "?" "?" "?"))))
+
 ;; Part 2 solution for input CODE
 (define (part-two code)
-  ;; TO-DO: Iterate over possible nouns and verbs
-  (values 0 1 3))
+  (noun-verb-iter code 0 0))
 
 ;; Print results
 (define (main args)
